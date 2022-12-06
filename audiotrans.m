e3aa55ce95bd11993aff91e9ff726b7b9cb90699
florian-devin <florian.devin13@gmail.com>
2022-12-06 10:38:17 +0100

    rx started

--- audiotrans.m
+++ audiotrans.m
@@ -19,13 +19,15 @@ conf.audiosystem = 'bypass'; % Values: 'matlab','native','bypass'
 conf.nbcarriers = 256;
 conf.carriersSpacing = 5; % Hz
 conf.cp_length = conf.nbcarriers / 2;
+conf.bandwidth = ceil((conf.nbcarriers + 1)/ 2)*conf.carriersSpacing;
+conf.nbdatapertrainning = 4;
 
 conf.f_s     = 48000;   % sampling rate  
 conf.f_sym   = 100;     % symbol rate
 conf.rolloff = 0.22;
 conf.filterlength = 20;
 conf.nframes = 1;       % number of frames to transmit
-conf.nbits   = 4*conf.nbcarriers*2;    % number of bits 
+conf.nbits   = conf.nbdatapertrainning*conf.nbcarriers*2;    % number of bits 
 conf.modulation_order = 2; % BPSK:1, QPSK:2
 conf.f_c     = 4000;
 
